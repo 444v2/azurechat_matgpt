7eab5f24abb91603b7a75898221eafcee8c9d627
John Simpson <jsimpson@midamericatransplant.org>
2023-12-16 02:33:34 -0600

    Different approach

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,14 @@ import { redirect } from "next/navigation";
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  const user = await userSession();
-  if (user) {
-    redirect("/chat");
-  }
-  return (
-    <Card className="h-full flex-1 overflow-hidden relative items-center justify-center flex">
-      <p>Redirecting to sign-in...</p>
-    </Card>
-  );
+  redirect("/chat");
+//  const user = await userSession();
+//  if (user) {
+//    redirect("/chat");
+//  }
+//  return (
+//    <Card className="h-full flex-1 overflow-hidden relative items-center justify-center flex">
+//      <LogIn />
+//    </Card>
+//  );
 }
