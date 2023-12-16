dfaac2cc51aa3bd031b2fa0d5182d91909be17a9
John Simpson <jsimpson@midamericatransplant.org>
2023-12-16 03:01:17 -0600

    login removal / set up admin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,44 @@
-import { LogIn } from "@/components/login/login";
+"use client";
+import { useEffect, useState } from 'react';
+import { signIn } from 'next-auth/react';
 import { Card } from "@/components/ui/card";
-import { signIn } from "next-auth/react";
-import { userSession } from "@/features/auth/helpers";
-import { redirect } from "next/navigation";
 
-export const dynamic = "force-dynamic";
+export default function Home() {
+    const [isClient, setIsClient] = useState(false);
 
-export default async function Home() {
-  const user = await userSession();
-  if (user) {
-    redirect("/chat");
-  }
+    useEffect(() => {
+        setIsClient(typeof window !== 'undefined');
+    }, []);
+
+    useEffect(() => {
+        if (isClient) {
+            signIn("azure-ad");
+        }
+    }, [isClient]);
+
     return (
-        signIn("azure-ad")
-  );
+        <Card className="h-full flex-1 overflow-hidden relative items-center justify-center flex">
+            <p>Redirecting to sign-in...</p>
+        </Card>
+    );
 }
+
+
+//import { LogIn } from "@/components/login/login";
+//import { Card } from "@/components/ui/card";
+//import { userSession } from "@/features/auth/helpers";
+//import { redirect } from "next/navigation";
+
+//export const dynamic = "force-dynamic";
+
+//export default async function Home() {
+//    const user = await userSession();
+//    if (user) {
+//        redirect("/chat");
+//    }
+//    return (
+//        <Card className="h-full flex-1 overflow-hidden relative items-center justify-center flex">
+//            <LogIn />
+//        </Card>
+//    );
+//}
