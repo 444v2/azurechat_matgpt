ce43f1d37fc4ad5ad60b49650030876ebe38c829
John Simpson <jsimpson@midamericatransplant.org>
2023-12-16 02:14:20 -0600

    again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,29 @@
-use client; 
+// page.tsx
+use client; // This directive marks the component as a Client Component
 
-import { LogIn } from "@/components/login/login";
 import { useEffect } from 'react';
+import { signIn } from 'next-auth/react';
 import { Card } from "@/components/ui/card";
-import { signIn } from "next-auth/react";
-import { userSession } from "@/features/auth/helpers";
-import { redirect } from "next/navigation";
-
-export const dynamic = "force-dynamic";
 
 export default function Home() {
     useEffect(() => {
         signIn("azure-ad");
     }, []);
 
-    // Optionally, you can render a loading message or similar
     return (
         <Card className="h-full flex-1 overflow-hidden relative items-center justify-center flex">
             <p>Redirecting to sign-in...</p>
         </Card>
     );
 }
+
+
+//import { LogIn } from "@/components/login/login";
+//import { Card } from "@/components/ui/card";
+//import { userSession } from "@/features/auth/helpers";
+//import { redirect } from "next/navigation";
+
+//export const dynamic = "force-dynamic";
 
 //export default async function Home() {
 //  const user = await userSession();
