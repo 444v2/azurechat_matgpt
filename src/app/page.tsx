e21aa08929d4479a3b3cffff07d4ce868c026e77
John Simpson <jsimpson@midamericatransplant.org>
2023-12-16 02:41:54 -0600

    login removal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,17 @@
 import { LogIn } from "@/components/login/login";
 import { Card } from "@/components/ui/card";
+import { signIn } from "next-auth/react";
 import { userSession } from "@/features/auth/helpers";
 import { redirect } from "next/navigation";
 
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  redirect("/chat");
-//  const user = await userSession();
-//  if (user) {
-//    redirect("/chat");
-//  }
-//  return (
-//    <Card className="h-full flex-1 overflow-hidden relative items-center justify-center flex">
-//      <LogIn />
-//    </Card>
-//  );
+  const user = await userSession();
+  if (user) {
+    redirect("/chat");
+  }
+    return (
+        signIn("azure-ad")
+  );
 }
