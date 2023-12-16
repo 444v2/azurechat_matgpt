29cf877e5f5781878be979b5a2cc9450eab2ffbe
John Simpson <jsimpson@midamericatransplant.org>
2023-12-16 02:01:49 -0600

    Skip the login button.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,33 @@
-import { signIn } from "next-auth/react";
 import { LogIn } from "@/components/login/login";
+import { useEffect } from 'react';
 import { Card } from "@/components/ui/card";
+import { signIn } from "next-auth/react";
 import { userSession } from "@/features/auth/helpers";
 import { redirect } from "next/navigation";
 
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  signIn("azure-ad");
+    useEffect(() => {
+        signIn("azure-ad");
+    }, []);
 
-  const user = await userSession();
-  if (user) {
-    redirect("/chat");
-  }
-  return (
-    <Card className="h-full flex-1 overflow-hidden relative items-center justify-center flex">
-      <LogIn />
-    </Card>
-  );
+    // Optionally, you can render a loading message or similar
+    return (
+        <Card className="h-full flex-1 overflow-hidden relative items-center justify-center flex">
+            <p>Redirecting to sign-in...</p>
+        </Card>
+    );
 }
+
+//export default async function Home() {
+//  const user = await userSession();
+//  if (user) {
+//    redirect("/chat");
+//  }
+//  return (
+//    <Card className="h-full flex-1 overflow-hidden relative items-center justify-center flex">
+//      <LogIn />
+//    </Card>
+//  );
+//}
