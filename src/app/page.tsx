079316a8142048a73b21ab378b4e30baa776cbe9
John Simpson <jsimpson@midamericatransplant.org>
2023-12-16 02:25:43 -0600

    Back to basics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,18 @@
-use client; // This directive marks the component as a Client Component
-
-import { useEffect } from 'react';
-import { signIn } from 'next-auth/react';
+import { LogIn } from "@/components/login/login";
 import { Card } from "@/components/ui/card";
+import { userSession } from "@/features/auth/helpers";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-    useEffect(() => {
-        signIn("azure-ad");
-    }, []);
+export const dynamic = "force-dynamic";
 
-    return (
-        <Card className="h-full flex-1 overflow-hidden relative items-center justify-center flex">
-            <p>Redirecting to sign-in...</p>
-        </Card>
-    );
+export default async function Home() {
+  const user = await userSession();
+  if (user) {
+    redirect("/chat");
+  }
+  return (
+    <Card className="h-full flex-1 overflow-hidden relative items-center justify-center flex">
+      <p>Redirecting to sign-in...</p>
+    </Card>
+  );
 }
-
-
-//import { LogIn } from "@/components/login/login";
-//import { Card } from "@/components/ui/card";
-//import { userSession } from "@/features/auth/helpers";
-//import { redirect } from "next/navigation";
-
-//export const dynamic = "force-dynamic";
-
-//export default async function Home() {
-//  const user = await userSession();
-//  if (user) {
-//    redirect("/chat");
-//  }
-//  return (
-//    <Card className="h-full flex-1 overflow-hidden relative items-center justify-center flex">
-//      <LogIn />
-//    </Card>
-//  );
-//}
