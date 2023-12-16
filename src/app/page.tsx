089325a391f3bca0333cf403d415989cd22dceab
John Simpson <jsimpson@midamericatransplant.org>
2023-12-16 01:22:22 -0600

    Skip login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { LogIn } from "@/components/login/login";
+import { signIn } from "next-auth/react";
 import { Card } from "@/components/ui/card";
 import { userSession } from "@/features/auth/helpers";
 import { redirect } from "next/navigation";
@@ -7,12 +8,14 @@ export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const user = await userSession();
-  if (user) {
-    redirect("/chat");
-  }
-  return (
-    <Card className="h-full flex-1 overflow-hidden relative items-center justify-center flex">
-      <LogIn />
-    </Card>
-  );
+    if (user) {
+        redirect("/chat");
+    } else {
+        signIn("azure-ad");
+    }
+//  return (
+//    <Card className="h-full flex-1 overflow-hidden relative items-center justify-center flex">
+//      <LogIn />
+//    </Card>
+//  );
 }
